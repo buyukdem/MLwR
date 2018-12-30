d55f6b31ff4da18b96348e04c5365112b571525e
Emre Buyuk <emrebuyuk@EMREs-MacBook-Air.local>
2018-12-30 17:49:36 +0300

    update

--- chapter4.R
+++ chapter4.R
@@ -68,12 +68,13 @@ sms_train <- apply(sms_train, 2, convert_counts)
 sms_test <- apply(sms_test, 2, convert_counts)
 
 #training a model
-#install.packages("e1071")
+install.packages("e1071")
 library(e1071)
 sms_classifier <- naiveBayes(sms_train, sms_raw_train$type)
 
 #evaluating model performance
 sms_test_pred <- predict(sms_classifier, sms_test)
+install.packages("gmodels")
 library(gmodels)
 CrossTable(sms_test_pred, sms_raw_test$type, 
            prop.chisq = F, prop.t = F, 
@@ -85,3 +86,55 @@ sms_test_pred <- predict(sms_classifier2, sms_test)
 CrossTable(sms_test_pred, sms_raw_test$type, 
            prop.chisq = F, prop.t = F, 
            dnn = c("predicted","actual"))
+
+#evaluating model performance with different metrics
+predicted_prob <- predict(sms_classifier2, sms_test, type = "raw")
+head(predicted_prob)
+sms_results <- data.frame(actual_type = sms_raw_test$type, 
+                          predict_type = sms_test_pred, 
+                          prob_spam = predicted_prob[,2])
+head(sms_results)
+head(subset(sms_results, actual_type != predict_type))
+
+#discerning one class versus all others, class of interest: positive class, others: negative class
+table(sms_results$actual_type, sms_results$predict_type)
+CrossTable(sms_results$actual_type, sms_results$predict_type)
+#accuracy = (TP+TN)/(TP+TN+FP+FN), error_rate = (FP+FN)/(TP+TN+FP+FN) = 1 - accuracy
+
+library(caret)
+confusionMatrix(sms_results$actual_type, sms_results$predict_type, positive = "spam")
+#gives you all the metrics, must specify the positive class
+
+install.packages("vcd")
+library(vcd)
+Kappa(table(sms_results$actual_type,sms_results$predict_type)) #when there are 2 classes weighted and unweighted are same
+install.packages("irr")
+library(irr)
+kappa2(sms_results[1:2])
+#kappa: adjusts accuracy by accounting for the possibility of a correct prediction by chance alone
+#kappa range: [0,1], 0.8-1 is very good agreement
+
+sensitivity(sms_results$predict_type, sms_results$actual_type, positive = "spam") #83.6% of spam messages classified correctly
+specificity(sms_results$predict_type, sms_results$actual_type, negative = "ham") #99.6% of ham messages classified correctly
+#sensitivty and specifity gives us the tradeoff between overly conservative (high sensitivity) and overly aggressive (high specifity) decision making
+#sensitivity = TP rate, specificity = TN rate
+#sensitivity = TP/(TP+FN), specificty = TN/(TN+FP)
+
+library(caret)
+posPredValue(sms_results$predict_type, sms_results$actual_type, positive = "spam")
+#precision = TP/(TP+FP) a precise model will predict the positive class correctly most of the time
+#sensitivity = recall
+#F-measure = (2 x precision x recall)/(recall + precision), combines the precision and recall and gives one metric to gauge performance
+
+install.packages("ROCR")
+library(ROCR)
+pred <- prediction(predictions = sms_results$prob_spam, labels = sms_results$actual_type)
+perf <- performance(pred, measure = "tpr", x.measure = "fpr")
+plot(perf, main = "ROC curve for spam filter",
+     col = "blue", lwd = 3)
+abline(a=0, b=1, lwd = 3, lty = 2)
+#fp rate = 1-specifity (x-axis), tp rate = sensitivity (y-axis)
+perf.auc <- performance(pred, measure = "auc")
+str(perf.auc)
+unlist(perf.auc@y.values) #area under ROC curve
+
--- chapter5.R
+++ chapter5.R
@@ -23,7 +23,7 @@ credit_test <- credit_rand[901:1000, ]
 prop.table(table(credit_train$default)); prop.table(table(credit_test$default))
 
 #training a model
-#install.packages("C50")
+install.packages("C50")
 library(C50)
 credit_model <- C5.0(credit_train[-17], credit_train$default)
 credit_model
@@ -54,3 +54,37 @@ credit_cost_pred <- predict(credit_cost, credit_test)
 CrossTable(credit_test$default, credit_cost_pred,
            prop.c = F, prop.chisq = F, prop.t = F,
            dnn = c("actual_default","predicted_default"))
+
+#partitioning data with holdout method
+random_ids <- order(runif(1000))
+credit_train2 <- credit[random_ids[1:500], ]
+credit_validation2 <- credit[random_ids[501:750], ]
+credit_test2 <- credit[random_ids[751:1000], ]
+prop.table(table(credit_train2$default)); prop.table(table(credit_validation2$default)); prop.table(table(credit_test2$default));
+
+#in some cases, a class may have very different proportions in each partition
+#stratified random sampling is used to address this problem
+library(caret)
+in_train <- createDataPartition(credit$default, p = 0.75, list = FALSE)
+credit_train3 <- credit[in_train, ]
+credit_test3 <- credit[-in_train, ]
+
+#k-fold cross-validation: a repeated holdout method
+#empirical evidence suggests that k > 10 has little added benefit to the model performance
+#mostly 10-fold cv is used
+set.seed(123)
+folds <- createFolds(credit$default, k = 10) 
+str(folds)
+#there is no function to perform cv so a loop is needed to automate the model construction and performance evaluation
+cv_results <- lapply(folds, function(x) {
+  credit_train_cv <- credit[x, ]
+  credit_test_cv <- credit[-x, ]
+  credit_model_cv <- C5.0(default ~ ., data = credit_train_cv)
+  credit_pred_cv <- predict(credit_model_cv, credit_test_cv)
+  credit_actual <- credit_test_cv$default
+  kappa <- kappa2(data.frame(credit_actual, credit_pred_cv))$value
+  return(kappa)
+})
+str(cv_results)
+mean(unlist(cv_results)) #cv suggests it is actually a poor model
+
